7be25136382448d127acec93f93ea6c0ff63ac72
Igor Desktop <igfigueiredo@outlook.com>
2021-06-18 14:04:48 +0200

    Styling tweaks

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,10 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Dutch City Tours</h1>
+      <div className="menu-burger">
+        <label for="toggle">&#9776;</label>
+        <input type="checkbox" id="toggle"/>
+      </div>
       <ul className="nav-links">
         <li>
           <a href="/" className="nav-link">home</a>
