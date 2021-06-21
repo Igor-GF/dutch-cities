95f6a990f974445d1e878202e09d9c32610126b3
Igor Desktop <igfigueiredo@outlook.com>
2021-06-21 22:20:48 +0200

    hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,21 +5,25 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Dutch City Tours</h1>
-      <div className="menu-burger">
-        <label for="toggle">&#9776;</label>
-        <input type="checkbox" id="toggle"/>
+
+      <div className="navbar-menu">
+        <div className="menu-burger">
+          <label for="toggle">&#9776;</label>
+          <input type="checkbox" id="toggle"/>
+        </div>
+
+        <ul className="nav-links">
+          <li>
+            <a href="/" className="nav-link">home</a>
+          </li>
+          <li>
+            <a href="/" className="nav-link">about</a>
+          </li>
+          <li>
+            <a href="/" className="nav-link active">tours</a>
+          </li>
+        </ul>
       </div>
-      <ul className="nav-links">
-        <li>
-          <a href="/" className="nav-link">home</a>
-        </li>
-        <li>
-          <a href="/" className="nav-link">about</a>
-        </li>
-        <li>
-          <a href="/" className="nav-link active">tours</a>
-        </li>
-      </ul>
     </nav>
   )
 }
